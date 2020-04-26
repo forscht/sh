0ef6ff9f7cd5daec629de77edc4f10a09cb76578
iroguesniper <59018146+iroguesniper@users.noreply.github.com>
2020-04-26 14:09:05 +0530

    Update install_node12_debian_distro.sh

--- install_node12_debian_distro.sh
+++ install_node12_debian_distro.sh
@@ -3,4 +3,4 @@ sudo apt update
 sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt -y install nodejs
-sudo apt-get install gcc g++ make
+sudo apt-get install gcc g++ make -y
