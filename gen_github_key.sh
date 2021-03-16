47fa4546f6aa09e7c307e29a547a10da961e3e3f
iroguesniper <59018146+iroguesniper@users.noreply.github.com>
2021-03-16 14:03:02 +0530

    Update gen_github_key.sh

--- gen_github_key.sh
+++ gen_github_key.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh-keygen -t rsa -b 4096 -C "darshan.stepin@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "iroguesniper@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
